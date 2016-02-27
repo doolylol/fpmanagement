b954f0a56540641a7d331b64186129183e9e20ca
nicklasholm <nicklas@njonline.dk>
2016-02-27 13:37:31 +0100

    Inital commit

---
+++ src/main/java/com/fitproject/FpmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.fitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpmanagementApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fitproject/FpmanagementApplicationTests.java
@@ -0,0 +1,18 @@
+package com.fitproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FpmanagementApplication.class)
+@WebAppConfiguration
+public class FpmanagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
